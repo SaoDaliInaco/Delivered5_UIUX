ee8d88eb9bbe6e06a7969b61f877ef28490657c8
SaoDaliInaco <dalyinacosao@gmail.com>
2026-01-11 14:48:29 +0700

    updated

---
+++ checkout2.js
@@ -0,0 +1,6 @@
+document.getElementById("checkoutBtn").addEventListener("click", function() {
+    document.getElementById("orderPopup").style.display = "flex";
+});
+document.getElementById("goHomeBtn").addEventListener("click", function() {
+    window.location.href = "HomePage.html";
+});
